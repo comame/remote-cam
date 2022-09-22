0c8a10f9fbff901294ca611c1584c75b1592215c
comame <dev@comame.xyz>
2022-09-23 01:09:39 +0900

    resolution

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
 const cameraButton = document.getElementById('camera')
 const offerButton = document.getElementById('offer')
-const adjustButton = document.getElementById('adjust')
 /** @type { HTMLVideoElement } */
-const videoElement = document.getElementById('video')
+const videoElement = document.getElementById('video') /** @type { HTMLVideoElement } */
+/** @type { HTMLSelectElement } */
 const selectElement = document.getElementById('select')
+
+const defaultResolutions = [
+    [ 'USB3.0 Capture Video ', 1920, 1080 ]
+]
 
 let sendVideo = false
 
@@ -17,6 +21,15 @@ function postMessage(message) {
 let peer = null
 /** @type { MediaStream } */
 let mediaStream = null
+
+navigator.permissions.query({ name: 'camera' }).then(status => {
+    switch (status.state) {
+        case 'denied': {
+            alert('Camera permission required')
+            break
+        }
+    }
+ })
 
 navigator.mediaDevices.enumerateDevices().then((devices) => {
     devices.filter(it => it.kind === 'videoinput').forEach(videoDevice => {
@@ -51,11 +64,23 @@ signaling.addEventListener('message', async (e) => {
 cameraButton.addEventListener('click', async () => {
     sendVideo = true
 
+    const videoConstraint = {
+        deviceId: selectElement.value
+    }
+
+    const defaultResolution = defaultResolutions
+        .find(r => selectElement.options[selectElement.selectedIndex].text.startsWith(r[0]))
+
+    console.log(defaultResolution)
+
+    if (defaultResolution) {
+        videoConstraint.width = defaultResolution[1]
+        videoConstraint.height = defaultResolution[2]
+    }
+
     const stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
-        video: {
-            deviceId: selectElement.value
-        }
+        video: videoConstraint
     })
 
     mediaStream = stream
@@ -64,7 +89,6 @@ cameraButton.addEventListener('click', async () => {
     videoElement.play()
 
     const { width, height, frameRate } = stream.getVideoTracks()[0].getSettings()
-    console.log(frameRate, width, height)
     videoElement.width = width
     videoElement.height = height
 })
@@ -109,21 +133,6 @@ async function preparePeerConnection() {
         }
         postMessage(message)
     })
-}
-
-adjustButton.addEventListener('click', () => {
-    adjustVideoSize()
-})
-
-function adjustVideoSize() {
-    const transceivers = peer.getTransceivers()
-    const videoTransceivers = transceivers.filter(it => it.receiver.track.kind === 'video')
-    if (videoTransceivers.length === 0) {
-        return
-    }
-    const { width, height } = videoTransceivers[0].receiver.track.getSettings()
-    videoElement.width = width
-    videoElement.height = height
 }
 
 async function handleOffer(offer) {
